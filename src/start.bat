2cb399a2bc3839ba44d3bad258c97b0f56a04706
Jara <jara@mivabe.nl>
2016-02-15 00:32:06 +0100

    Update start.bat

--- src/start.bat
+++ src/start.bat
@@ -1,5 +1,13 @@
-for /l %%i in (1, 1, 500) do (
-	cls
-	node index.js
-	pause
-)
+@echo off
+
+rem *** Please make sure node can be accessed at this phase, so either do a path to node. (see Dos manuals)
+rem *** or replace node by for example c:\path.to.where.node.is\node or if you wish to use JX replace node
+rem *** with JX.
+rem *** exemple: C:\JXCore\JX install..... etc
+
+:loop
+   cls
+   node install ws http fs sys mysql url underscore
+   node install -g n
+   node index.js
+   goto loop
