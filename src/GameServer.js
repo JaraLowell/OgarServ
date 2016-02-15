5ec064b1deae15be26df5b732802ead4ad9a5444
Jara <jara@mivabe.nl>
2016-02-15 01:17:19 +0100

    500 now... Makan....

--- src/GameServer.js
+++ src/GameServer.js
@@ -1022,7 +1022,7 @@ GameServer.prototype.updateCells = function () {
                 cell.mass *= massDecay;
             } else {
                 // Faster decay when bigger then 5k
-                cell.mass *= massDecay - ( this.config.FastDecayMultiplier / 250);
+                cell.mass *= massDecay - ( this.config.FastDecayMultiplier / 500);
             }
         }
     }
