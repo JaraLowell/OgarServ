8c06026cccb74fd4618cc5c71d6a3a83b1d2a09d
Jara <jara@mivabe.nl>
2017-04-10 18:46:47 +0200

    To many issues with new vs old node.js
    As people not read a wiki or the likes we remove the --max-new-space
    options as these are for an older version of Node.JS and as people have
    no clue apparently how to edit a bat file....

--- src/runnode.sh
+++ src/runnode.sh
@@ -2,13 +2,13 @@
 clear
 if [ ! -d "node_modules" ]; then
   echo module files not found, installing them now...
-  npm install ws http fs sys mysql url underscore quad-node vector2-node xmlhttprequest punycode
+  npm install quad-node vector2-node ws http fs sys mysql xmlhttprequest punycode
   sleep 2
 fi
 
 while true; do
   clear
-  node --nouse-idle-notification --expose-gc --always-compact --max-new-space-size=2048 --max-old-space-size=2048 index.js
+  node --nouse-idle-notification --expose-gc --always-compact index.js
   echo Server went off, waiting ten sec then restarting it...
   sleep 30
 done
--- src/start.bat
+++ src/start.bat
@@ -5,7 +5,10 @@ rem *** or replace node by for example c:\path.to.where.node.is\node or if you w
 rem *** with JX.
 rem *** exemple: C:\JXCore\JX install..... etc
 
+rem *** Using NPM for now
+npm install quad-node vector2-node ws http fs sys mysql xmlhttprequest punycode
+
 :loop
    cls
-   node --nouse-idle-notification --expose-gc --always-compact --max-new-space-size=2048 --max-old-space-size=2048 index.js
+   node --nouse-idle-notification --expose-gc --always-compact index.js
    goto loop
